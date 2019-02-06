439b1246ed7bd6ab84fdf015347e31525a1a6397
Conor <cwilson0005@hotmail.co.uk>
2019-02-06 10:53:32 +0000

    started working on select view

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+
+  const selectElement = document.querySelector('select#countries');
+  const countriesDropdown = new SelectView(selectElement);
+  countriesDropdown.bindEvents();
 });
